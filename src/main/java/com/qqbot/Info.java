815d4231aab4f76eeb70ab2a27e0663f131984d8
Thousand-Dust <2984613883@qq.com>
2022-12-23 02:31:11 +0800

    Initial commit

---
+++ src/main/java/com/qqbot/Info.java
@@ -0,0 +1,13 @@
+package com.qqbot;
+
+public class Info {
+
+    public static final long SuperManagerId = 2984613883L;
+
+    //群消息事件缓存最大大小
+    public static final int EVENT_CACHE_MAX_SIZE = 1024;
+
+    //群刷屏检测引用的历史消息数量
+    public static final int CHECK_EVENT_COUNT = 20;
+
+}
