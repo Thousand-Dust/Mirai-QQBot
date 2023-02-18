9bd5632ffdbb94234849393a0586731ce9d2d5c4
Thousand-Dust <2984613883@qq.com>
2023-02-19 02:19:01 +0800

    23/2/19

--- src/main/java/com/qqbot/group/msg/ProcConfig.kt
+++ src/main/java/com/qqbot/group/msg/ProcConfig.kt
@@ -6,12 +6,11 @@ import com.qqbot.Info
 
 /**
  * 群消息处理器系统配置的封装
+ * TODO: 未完成
  */
 class ProcConfig(val groupId: Long) {
 
     //配置文件路径
     private val configPath = Info.DATA_ROOT_PATH+"/Group/${groupId}/procConfig.json"
-
-
 
 }
