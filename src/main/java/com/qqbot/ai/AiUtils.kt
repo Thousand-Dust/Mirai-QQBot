ba808907d6f45e1fe9cdf557a89db3dce7168ca9
Thousand-Dust <2984613883@qq.com>
2023-03-16 18:20:06 +0800

    封装了几个用于半监督训练的方法

--- src/main/java/com/qqbot/ai/AiUtils.kt
+++ src/main/java/com/qqbot/ai/AiUtils.kt
@@ -1,5 +1,11 @@
 package com.qqbot.ai
 
+import com.qqbot.Utils
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.stream.Collectors
+
 /**
  * 将@的QQ号删除
  */
@@ -24,4 +30,79 @@ fun String.formatForClassifier(): String {
         }
     }
     return result
+}
+
+/**
+ * 删除文件中重复的行
+ */
+fun deleteDuplicateLines(readPath: String, outPath: String) {
+    //先将文件中每行重复的内容删掉输出到另一个文件
+    val fileInput = FileInputStream(readPath)
+    val fileOutput = FileOutputStream(outPath)
+    val lines = fileInput.bufferedReader().lines().collect(Collectors.toList())
+    val set = HashSet<String>()
+    for (line in lines) {
+        if (set.contains(line)) continue
+        set.add(line)
+        fileOutput.write((line + "\n").toByteArray())
+    }
+    fileInput.close()
+    fileOutput.close()
+}
+
+/**
+ * 将未分词的数据分词
+ */
+fun dataParticiple(classifier: TextClassifier, readPath: String, outPath: String) {
+    val dataIn = FileInputStream(readPath)
+    //将文件内容按行循环读取
+    dataIn.bufferedReader().useLines { lines ->
+        lines.forEach { line ->
+            //将每行按空格分割成两部分
+            val parts = line.split(" ", limit = 2)
+            //第一部分是类别
+            val label = parts[0]
+            //第二部分是文本
+            val text = parts[1]
+            //将文本格式化
+            val formattedText = classifier.participle(text)
+            //将格式化后的文本和类别保存到文件
+            Utils.writeFile(
+                outPath,
+                "$label $formattedText\n".toByteArray(),
+                true
+            )
+        }
+    }
+    dataIn.close()
+}
+
+/**
+ * 对数据进行修正，输出到新文件
+ */
+fun dataCorrection(classifier: TextClassifier, modelFile: File, dataFile: File, outDataFile: File) {
+    val dataIn = FileInputStream(dataFile)
+    val dataOut = FileOutputStream(outDataFile)
+    //将文件内容按行循环读取
+    dataIn.bufferedReader().useLines { lines ->
+        lines.forEach { line ->
+            //将每行按空格分割成两部分
+            val parts = line.split(" ", limit = 2)
+            //第一部分是类别
+            val label = parts[0]
+            //第二部分是文本
+            val text = parts[1]
+
+            //先将文本进行分词
+            val formatText = classifier.participle(text.removeAt())
+            //将文本格式化
+            val label1 = classifier.categorize(formatText.split(" ").toTypedArray()).first
+
+            //将重新分类后的类别和文本保存到文件
+            dataOut.write("$label1 $text\n".toByteArray())
+        }
+    }
+
+    dataIn.close()
+    dataOut.close()
 }
